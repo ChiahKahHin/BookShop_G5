04adde75862ddee836eae20ea84b6ed939bffc6d
Teng Wei Kang <weikangteng@gmail.com>
2021-05-02 21:34:41 +0800

    change "bookstore" to "bookshop"

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,10 +3,7 @@
 		<div class="row align-items-center justify-content-center content-center">
 			<div class="col-lg-6 mb-lg-0 mb-4">
 				<div class="copyright text-center text-sm text-muted w-100">
-					© <script>document.write(new Date().getFullYear())</script>,
-					made with <i class="fa fa-heart"></i> by
-					<a href="https://www.creative-tim.com" class="font-weight-bold" target="_blank">Creative Tim</a>
-					for a better web.
+					Copyright © <script>document.write(new Date().getFullYear())</script> Book Shop
 				</div>
 			</div>
 		</div>
