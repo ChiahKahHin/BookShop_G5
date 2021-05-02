04351f16d95d4b3997854a1d8ce60048fdbe47bc
Teng Wei Kang <weikangteng@gmail.com>
2021-05-02 18:02:20 +0800

    Center the footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,18 +1,13 @@
 <footer class="footer pt-3">
 	<div class="container-fluid">
-		<div class="row align-items-center justify-content-lg-between">
+		<div class="row align-items-center justify-content-center content-center">
 			<div class="col-lg-6 mb-lg-0 mb-4">
-				<center>
-					
-					<div class="copyright text-center text-sm text-muted text-lg-left">
-						© <script>
-								document.write(new Date().getFullYear())
-								</script>,
-						made with <i class="fa fa-heart"></i> by
-						<a href="https://www.creative-tim.com" class="font-weight-bold" target="_blank">Creative Tim</a>
-						for a better web.
-					</div>
-				</center>
+				<div class="copyright text-center text-sm text-muted w-100">
+					© <script>document.write(new Date().getFullYear())</script>,
+					made with <i class="fa fa-heart"></i> by
+					<a href="https://www.creative-tim.com" class="font-weight-bold" target="_blank">Creative Tim</a>
+					for a better web.
+				</div>
 			</div>
 		</div>
 	</div>
