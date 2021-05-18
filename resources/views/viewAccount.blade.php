c67b17cb50546812dd9420f758be5a1be454c409
UvuvuevuevueOsas <56707975+UvuvuevuevueOsas@users.noreply.github.com>
2021-05-18 15:43:23 +0800

    view account

---
+++ resources/views/viewAccount.blade.php
@@ -0,0 +1,13 @@
+@extends("layouts.template")
+
+@section('title')
+    Admin | View Account
+@endsection
+
+@section('navtitle')
+    View Account
+@endsection
+
+@section('content')
+
+@endsection
