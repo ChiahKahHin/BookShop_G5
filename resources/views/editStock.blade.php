7e66aefa08cb99af350f2ee74a5e893fa222dbfd
Teng Wei Kang <weikangteng@gmail.com>
2021-05-21 10:17:43 +0800

    Implemented edit stock in backend

--- resources/views/editStock.blade.php
+++ resources/views/editStock.blade.php
@@ -137,7 +137,7 @@
                                 </div>
                                 <div class="row">
                                     <div class="col-md-6">
-                                        <button class="btn bg-gradient-info w-100 mt-4 md-6" type="submit">Add stock</button>
+                                        <button class="btn bg-gradient-info w-100 mt-4 md-6" type="submit">Update Stock</button>
                                     </div>
                                 </div>
                             </div>
