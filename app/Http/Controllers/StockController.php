f4c88088b81e5a7f39e610e73a08236ff7e7456a
Teng Wei Kang <weikangteng@gmail.com>
2021-05-21 11:21:11 +0800

    Improve coding for edit stock backend

--- app/Http/Controllers/StockController.php
+++ app/Http/Controllers/StockController.php
@@ -79,8 +79,7 @@ class StockController extends Controller
         $stock->book_trade_price = request('book_trade_price_input');
         $stock->book_retail_price = request('book_retail_price_input');
         $stock->book_quantity = request('book_quantity_input');
-        $stock->save();
         $stock = Stock::findOrFail($isbn);
-        return redirect()->route("editStock", ["isbn" => $stock->book_isbn_no])->with("message", "Stock updated successfully");
+        return redirect()->route("editStock", ["isbn" => $isbn])->with("message", "Stock updated successfully");
     }
 }
