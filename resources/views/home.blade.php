f591c87f800b961cef9e43d3e17dd3ff9f0a2fcf
DinJun <dinjun@users.noreply.github.com>
2021-06-03 12:32:39 +0800

    Fixed CSS to Unique class for quantity selection in homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -50,35 +50,6 @@ Book Shop
     </button>
   </div>
 {{-- Carousel Caption End --}}
-
-{{-- Carousel Indicator --}}
-{{-- <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel" style="max-height: 35rem;">
-    <div class="carousel-indicators">
-      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-    </div>
-    <div class="carousel-inner" style="max-height: 35rem;">
-      <div class="carousel-item active">
-        <img src="{{ URL::asset('assets/img/book_bg.jpg') }}" class="d-block w-100" alt="...">
-      </div>
-      <div class="carousel-item">
-        <img src="{{ URL::asset('assets/img/book_bg1.jpg') }}" class="d-block w-100" alt="...">
-      </div>
-      <div class="carousel-item">
-        <img src="{{ URL::asset('assets/img/book_bg2.jpg') }}" class="d-block w-100" alt="...">
-      </div>
-    </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-    </button>
-</div> --}}
-{{-- Carousel Indicator End --}}
 @endsection
 
 @section("content")
