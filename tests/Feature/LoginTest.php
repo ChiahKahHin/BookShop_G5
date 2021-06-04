5b8ddc2db4cd5b5720ab89765edd268a7ef00b8e
Teng Wei Kang <weikangteng@gmail.com>
2021-06-04 10:04:53 +0800

    Create and run unit testing for login

---
+++ tests/Feature/LoginTest.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Http\Controllers\UserController;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Tests\TestCase;
+use Illuminate\Http\Request;
+
+class LoginTest extends TestCase
+{
+    use RefreshDatabase, WithFaker;
+
+    private $admin;
+    private $customer;
+
+    public function setUp(): void {
+        parent::setUp();
+        $this->admin = User::factory()->create(
+            [
+                'username' => "admin10",
+                'phone' => $this->faker->regexify("(\+6)?01[0-46-9]-[0-9]{7,8}"),
+                'email' => $this->faker->unique()->safeEmail,
+                'password' => Hash::make("p455w0rd"),
+                'role' => 0,
+                'remember_token' => Str::random(10),
+            ]
+        );
+        $this->admin->hidden_password = "p455w0rd";
+        $this->customer = User::factory()->create(
+            [
+                'username' => "customer",
+                'phone' => $this->faker->regexify("(\+6)?01[0-46-9]-[0-9]{7,8}"),
+                'email' => $this->faker->unique()->safeEmail,
+                'password' => Hash::make("p455w0rd"),
+                'role' => 1,
+                'remember_token' => Str::random(10),
+            ]
+        );
+        $this->customer->hidden_password = "p455w0rd";
+    }
+
+    public function tearDown(): void {
+        parent::tearDown();
+        unset($this->admin);
+        unset($this->customer);
+    }
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_load_login_page() {
+        $response = $this->get(route("login"));
+        
+        $response->assertOk();
+    }
+
+    public function test_admin_login() {
+        $admin = $this->admin;
+        $response = $this->followingRedirects()
+            ->actingAs($admin)
+            ->post(route("login"), [
+                "username" => $admin->username,
+                "password" => $admin->hidden_password
+            ]
+        )->assertSessionHasNoErrors()
+        ->assertOk();
+        $this->isAuthenticated();
+        $response->assertViewIs("dashboard");
+    }
+
+    public function test_customer_login() {
+        $customer = $this->customer;
+        $response = $this->followingRedirects()
+            ->actingAs($customer)
+            ->post(route("login"), [
+                "username" => $customer->name,
+                "password" => $customer->hidden_password
+            ]
+        )->assertSessionHasNoErrors()
+        ->assertOk();
+        
+        $this->isAuthenticated();
+        $response->assertViewIs("home");
+    }
+}
