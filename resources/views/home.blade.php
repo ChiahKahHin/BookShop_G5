0ea0fa59cb87d7b622492907c3c8802022aec2ad
KahHin <chiahkahhin@hotmail.com>
2021-06-10 19:41:46 +0800

    Remove extra text shown in home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -24,24 +24,24 @@
                 <img src="{{ URL::asset('assets/img/book_bg.jpg') }}" class="d-block w-100" alt="..."
                     style="max-height: 35rem;">
                 <div class="carousel-caption d-none d-md-block">
-                    <h5>First slide label</h5>
-                    <p>Some representative placeholder content for the first slide.</p>
+                    {{-- <h5>First slide label</h5> --}}
+                    {{-- <p>Some representative placeholder content for the first slide.</p> --}}
                 </div>
             </div>
             <div class="carousel-item">
                 <img src="{{ URL::asset('assets/img/book_bg1.jpg') }}" class="d-block w-100" alt="..."
                     style="max-height: 35rem;">
                 <div class="carousel-caption d-none d-md-block">
-                    <h5>Second slide label</h5>
-                    <p>Some representative placeholder content for the second slide.</p>
+                    {{-- <h5>Second slide label</h5> --}}
+                    {{-- <p>Some representative placeholder content for the second slide.</p> --}}
                 </div>
             </div>
             <div class="carousel-item">
                 <img src="{{ URL::asset('assets/img/book_bg2.jpg') }}" class="d-block w-100" alt="..."
                     style="max-height: 35rem;">
                 <div class="carousel-caption d-none d-md-block">
-                    <h5>Third slide label</h5>
-                    <p>Some representative placeholder content for the third slide.</p>
+                    {{-- <h5>Third slide label</h5> --}}
+                    {{-- <p>Some representative placeholder content for the third slide.</p> --}}
                 </div>
             </div>
         </div>
