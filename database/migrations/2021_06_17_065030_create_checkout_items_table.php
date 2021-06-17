bbb87ff74122f7b6ebd663479472aa711062d971
DinJun <dinjun@users.noreply.github.com>
2021-06-17 15:20:08 +0800

    Added migration to add two additional tables for checkout

---
+++ database/migrations/2021_06_17_065030_create_checkout_items_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCheckoutItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('checkout_items', function (Blueprint $table) {
+            $table->id();
+            $table->integer('checkoutID');
+            $table->string('book_isbn_no');
+            $table->integer('book_quantity');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('checkout_items');
+    }
+}
